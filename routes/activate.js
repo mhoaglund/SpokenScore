71e8166d279940e085e9dd1acace8d3cf45ae82e
mhoaglund <maxwell.hoaglund@gmail.com>
2016-03-23 14:11:58 -0500

    renamed extract module to 'score'.

--- routes/activate.js
+++ routes/activate.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var twilio = require('twilio');
 var TwilioClient = global.TwilioClient;
 var async = require('async');
-var extract = require('../extract.js'); //http://stackoverflow.com/questions/5797852/in-node-js-how-do-i-include-functions-from-my-other-files
+var score = require('../score.js'); //http://stackoverflow.com/questions/5797852/in-node-js-how-do-i-include-functions-from-my-other-files
 
 router.get('/', function (req, res) {
         if (!global.isStarted) {
@@ -13,7 +13,6 @@ router.get('/', function (req, res) {
                 });
             });
             global.isStarted = true;
-            //extract.scoreSet();
         }
     res.render('index', { title: 'Express' });
 });
--- score.js
+++ score.js
@@ -1,7 +1,7 @@
 var fs = require("fs");
 
 module.exports = {
-    scoreSet: function(){
+    Score: function(){
         var obj;
         fs.readFile('./score.json', 'utf8', function (err, data) {
             if (err) throw err;
